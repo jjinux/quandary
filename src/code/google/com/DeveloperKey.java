dab50ac68487b20d9168aa9491ef8f60b48c3bf1
Shannon -jj Behrens <jjinux@gmail.com>
2012-07-07 13:53:46 +0100

    Committed my developer key

---
+++ src/code/google/com/DeveloperKey.java
@@ -0,0 +1,5 @@
+package code.google.com;
+
+public class DeveloperKey {
+    public static final String DEVELOPER_KEY = "AI39si5NCZgFl3y0IFbV3W_lvIdvrYqWzxx96jj8B29tgYEW_8lTEdJSM86B7HfsgwAwxe2Mht_B7j_Q_r771hE2eVVeWG0xcQ";
+}
